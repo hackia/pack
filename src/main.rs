38180add9674f51cde8ce9fc59ed8de86acfd03e
hackia <dev@hackia.org>
2025-10-29 20:12:11 +0100

    Nebula  ~ rename project to k
    	Why changes?
    
    		* rename project pack to k
    
    	Breaking Changes:
    
    		* yes the url it's no the same
    
    	What changes?
    
    		* arename project pack to k
    
    	Who changes?
    
    		* @hackia ~ Developer
    
    	Benefits:
    
    		* k pack --if=. --of=a
    		* k unpack --archive=a --destination=/usr/src/a
    		* can be possible
    
    	Notes:
    
    		* no implemented for the moment
    
    	Resolves
    
    		Fixes #4

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use clap::{Arg, ArgMatches, Command};
+
+fn cli() -> ArgMatches {
+    Command::new("k")
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("A tool for packing files")
+        .subcommand(
+            Command::new("unpack")
+                .about("Unpack files")
+                .arg(
+                    Arg::new("archive")
+                        .short('a')
+                        .long("archive")
+                        .help("the archives to unpack")
+                        .required(true),
+                )
+                .arg(
+                    Arg::new("destination")
+                        .short('d')
+                        .long("destination")
+                        .help("the destination directory")
+                        .required(true),
+                ),
+        )
+        .subcommand(
+            Command::new("pack")
+                .about("Pack files")
+                .arg(
+                    Arg::new("input")
+                        .long("if")
+                        .require_equals(true)
+                        .help("the directory to pack")
+                        .required(true),
+                )
+                .arg(
+                    Arg::new("output")
+                        .long("of")
+                        .require_equals(true)
+                        .help("The destination file")
+                        .required(true),
+                ),
+        )
+        .get_matches()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches: ArgMatches = cli();
+
+    if matches.subcommand_matches("unpack").is_some() {
+        println!("Unpacking files...");
+        // Handle unpack command
+    } else {
+        println!("Packing files...");
+        // Handle pack command
+    }
 }
