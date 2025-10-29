925b820668d1557336e378a4be5a3d0b520899ab
hackia <dev@hackia.org>
2025-10-29 20:30:40 +0100

    Big Bang  ~ parse user input
    	Why changes?
    
    		* in order to create archive i must be have files to archive
    
    	Breaking Changes:
    
    		* no
    
    	What changes?
    
    		* inmplement user parse input to get files to pack
    
    	Who changes?
    
    		* @hackia ~ Developer
    
    	Benefits:
    
    		* receve correct info
    
    	Notes:
    
    		* no implemented complet in progress
    
    	Resolves
    
    		Fixes #4

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use clap::{Arg, ArgMatches, Command};
 
 fn cli() -> ArgMatches {
@@ -43,14 +45,43 @@ fn cli() -> ArgMatches {
         .get_matches()
 }
 
+#[must_use]
+pub fn files(directory: &str) -> Vec<PathBuf> {
+    let mut files: Vec<PathBuf> = Vec::new();
+    let mut w = ignore::WalkBuilder::new(directory);
+    w.add_custom_ignore_filename(".packignore");
+    w.same_file_system(true);
+    let walker: ignore::Walk = w.build();
+    for result in walker.flatten() {
+        let path = result.path();
+        if path.is_file() {
+            files.push(path.to_path_buf());
+        }
+    }
+    files
+}
 fn main() {
     let matches: ArgMatches = cli();
 
-    if matches.subcommand_matches("unpack").is_some() {
-        println!("Unpacking files...");
-        // Handle unpack command
-    } else {
-        println!("Packing files...");
+    if matches.subcommand_matches("pack").is_some() {
+        if let Some(sub_m) = matches.subcommand_matches("pack") {
+            let input = sub_m.get_one::<String>("input").unwrap();
+            let output = sub_m.get_one::<String>("output").unwrap();
+            let files_to_pack = files(input);
+            println!("Files to pack from '{input}': {files_to_pack:?}");
+            println!("Output will be saved to '{output}'");
+            println!("Packing files...");
+        }
         // Handle pack command
+    } else if matches.subcommand_matches("unpack").is_some() {
+        if let Some(sub_m) = matches.subcommand_matches("unpack") {
+            let archive = sub_m.get_one::<String>("archive").unwrap();
+            let destination = sub_m.get_one::<String>("destination").unwrap();
+            println!("Unpacking archive '{archive}'");
+            println!("Files will be extracted to '{destination}'");
+            println!("Unpacking files...");
+        }
+    } else {
+        eprintln!("No valid subcommand was used.");
     }
 }
